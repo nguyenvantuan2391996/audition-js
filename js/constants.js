787753b290b879ee8abfe94e195613e5f89ef470
tuannguyen <nguyenvantuan2391996@gmail.com>
2022-06-12 01:06:01 +0700

    init

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+// Const variable
+const MAX_LEVEL = 11
+const MIN_COUNT_TO_PLAY = 5
+const LIST_KEY_HAS_REVERSE = ["right", "up", "down", "left", "right-reverse", "up-reverse", "down-reverse", "left-reverse"]
+const LIST_KEY = ["right", "up", "down", "left"]
+const MAP_KEY = new Map([["right", "right"], ["up", "up"], ["down", "down"], ["left", "left"], ["right-reverse", "left"], ["up-reverse", "down"], ["down-reverse", "up"], ["left-reverse", "right"],])
